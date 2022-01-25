0a9254c5db96d910750b72411ba5ad40a17c2778
AT <anand.tekriwa@cuanschutz.edu>
2022-01-25 08:00:18 -0700

    livescript update
    TTL and behavior output confirmed to match, analysis beginning, see X_LiveScript_V2

--- pdil/PDil_sandbox_V2.m
+++ pdil/PDil_sandbox_V2.m
@@ -30,7 +30,7 @@ end
 ptPlaceholder = 'pt_01';
 behaviorFolderName = 'pt_01_postOpDay2_PDil';
 behaviorFileName = 'pt_01_blockNum_1_computerTT_coop_taskoutput';
-ephysfilename = 'PO_Day_04.nwb';
+ephysfilename = 'PO_Day_02.nwb';
 
 %% BEHAVIOR DATA LOAD IN BELOW
 
@@ -40,25 +40,90 @@ load(behaviorFileName); %all behavioral structs should be loaded in as 'taskoutp
 
 %% Behavioral analysis
 
+%11/15/21 some notes to understand the taskoutput struct, refer to the end
+%of PDil_AT_V13_useWpts.m to see what task vars are used to calculate each,
+%if desired.
 
+%Introtime below takes us to up until the 'Starting study, please remember
+%you can stop at any time.' screen
+taskoutput.timing.Introtime;
+%below sumoftics var is actually the same thing as Introtime right above
+taskoutput.timing.timing_sumTictocs_Intro;
+
+%below is the 'toc' output from the wholesession 'tic', command was run
+%right before the timing command which led to the above two
+taskoutput.timing.timing_wholesession_Intro;
+%below is the exact same thing as above
+taskoutput.timing.Intro_wholesession;
+
+
+
+taskoutput.timing.timing_sumTictocs_introScreens = timing_sumTictocs_introScreens;
+taskoutput.timing.Time_scrn_flip_intro1 = Time_scrn_flip_intro1;
+taskoutput.timing.Time_scrn_flip_intro2 = Time_scrn_flip_intro2;
+taskoutput.timing.Time_scrn_flip_intro3 = Time_scrn_flip_intro3;
+taskoutput.timing.Time_postscrn_flip_intro1 = Time_postscrn_flip_intro1;
+taskoutput.timing.Time_postscrn_flip_intro2 = Time_postscrn_flip_intro2;
+taskoutput.timing.Time_postscrn_flip_intro3 = Time_postscrn_flip_intro3;
+taskoutput.timing.timing_wholesession_introScreens = timing_wholesession_introScreens;
+taskoutput.timing.IntroScreens_wholesession = IntroScreens_wholesession;
+taskoutput.timing.Intro_wholesession = Intro_wholesession;
+
+
+taskoutput.timing.Time_scrn_flip_trials1 = Time_scrn_flip_trials1;
+taskoutput.timing.Time_scrn_flip_trials2 = Time_scrn_flip_trials2;
+taskoutput.timing.Time_scrn_flip_trials3 = Time_scrn_flip_trials3;
+taskoutput.timing.Time_scrn_flip_trials4 = Time_scrn_flip_trials4;
+taskoutput.timing.Time_scrn_flip_trials5 = Time_scrn_flip_trials5;
+taskoutput.timing.Time_postscrn_flip_trials1 = Time_postscrn_flip_trials1;
+taskoutput.timing.Time_postscrn_flip_trials2 = Time_postscrn_flip_trials2;
+taskoutput.timing.Time_postscrn_flip_trials3 = Time_postscrn_flip_trials3;
+taskoutput.timing.Time_postscrn_flip_trials4 = Time_postscrn_flip_trials4;
+taskoutput.timing.Time_postscrn_flip_trials5 = Time_postscrn_flip_trials5;
+
+taskoutput.timing_wholesession_trial_n = timing_wholesession_trial_n;
+taskoutput.timing_sumTictocs_trial_n = timing_sumTictocs_trial_n;
+
+taskoutput.timing.timing_wholesession = toc(wholesession);
+
+for ik = 1:length(taskoutput.choice_matrix_player1)
+    if strcmp(taskoutput.choice_matrix_player1(ik), "c")
+taskoutput.choice_matrix_player1_logical(ik) = 1;
+    elseif strcmp(taskoutput.choice_matrix_player1(ik), "d")
+        taskoutput.choice_matrix_player1_logical(ik) = 0;
+    end
+end
+
+for ik = 1:length(taskoutput.choice_matrix_player2)
+    if strcmp(taskoutput.choice_matrix_player2(ik), "c")
+taskoutput.choice_matrix_player2_logical(ik) = 1;
+    elseif strcmp(taskoutput.choice_matrix_player2(ik), "d")
+        taskoutput.choice_matrix_player2_logical(ik) = 0;
+    end
+end
+
+
+
+
+
+
+
+
+%6/21/20 so the below is giving us the start and stop times for when
+%query1 and query2 are posed, following the 5th trial.
 %Task navigation
 for aa = 5
     timeTillStartQueries = taskoutput.timing.Time_scrn_flip_trials1(aa, 2)+taskoutput.timing.Time_scrn_flip_trials2(aa, 2)+taskoutput.timing.Time_scrn_flip_trials3(aa, 2)+taskoutput.timing.Time_scrn_flip_trials4(aa, 2)+taskoutput.timing.Time_scrn_flip_trials5(aa, 2)+taskoutput.timing.Time_postscrn_flip_trials1(aa,1)+taskoutput.timing.Time_postscrn_flip_trials2(aa,1)+taskoutput.timing.Time_postscrn_flip_trials3(aa,1)+taskoutput.timing.Time_postscrn_flip_trials4(aa,1)+taskoutput.timing.Time_postscrn_flip_trials5(aa,1);
 end
 
-
-
 timeTillEndQuery1 = taskoutput.timing.Time_query1(1) + taskoutput.timing.Time_query1_rxntime(1) + taskoutput.timing.Time_query1_Postrxntime(1);
-
-
 timeTillEndQuery2 = taskoutput.timing.Time_query2(1) + taskoutput.timing.Time_query2_rxntime(1) + taskoutput.timing.Time_query2_Postrxntime(1);
-
-
 sumforexample = timeTillStartQueries+timeTillEndQuery1+timeTillEndQuery2;
 
 
-%6/21/20 so the above is giving us the start and stop times for when
-%query1 and query2 are posed, following the 5th trial.
+
+
+
 
 
 
